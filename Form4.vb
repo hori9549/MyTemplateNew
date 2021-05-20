b1b5842b50d5cf86162d8ffa8f3c0369ed0cf21f
hori9549 <hori9549@gmail.com>
2021-05-20 13:33:45 +0900

    文字列補間式

--- Form4.vb
+++ Form4.vb
@@ -1,6 +1,17 @@
 ﻿Public Class frmEx2_2
     Private Sub Form4_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Console.WriteLine("Hello World!")
+        Console.WriteLine("string.Format を使って書くと...!")
+        Dim Name As String = "堀内"
+        Dim Hour As Integer = DateTime.Now.Hour
+        Dim Message As String = String.Format("こんにちは、{0}さん。今は{1}時です。", Name, Hour)
+        Console.WriteLine(Message)
+
+        Console.WriteLine(" 文字列補間式 $ を使って書くと")
+        Console.WriteLine(" 文字列の先頭に $ をつけるだけオーケー。_
+                            字列の中に直接埋め込むことができます。")
+        Console.WriteLine($"こんにちは、{Name}さん。 今は{Hour}時です。")
+
+
         Dim s1 As String = "foo"
         Dim s2 As String = "bar"
         Dim s3 As String = "baz"
