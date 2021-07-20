ca95a2893fc48a3a72a9b3ce1fd8791a6842ab74
hori9549 <hori9549@gmail.com>
2021-07-20 14:24:18 +0900

    継承の異本

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.MyTemplate.frmメニュー
+            Me.MainForm = Global.MyTemplate.Form5
         End Sub
     End Class
 End Namespace
