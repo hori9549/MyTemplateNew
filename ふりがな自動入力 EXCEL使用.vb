d608a09f9b5b9c2976e96b17afe3f30b1b3584dc
yoshiyuki <hori9549@gmail.com>
2021-07-22 08:12:48 +0900

    gouinnni動かしてる

--- ふりがな自動入力 EXCEL使用.vb
+++ ふりがな自動入力 EXCEL使用.vb
@@ -3,27 +3,27 @@
 ''' </summary>
 Public Class frmふりがな自動入力_EXCEL使用
     Private Sub ふりがな自動入力_EXCEL使用_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        AddHandler txt変換元.Enter, AddressOf KanjiToKana
+        Me.KeyPreview = True
+
+        AddHandler txt変換元.Enter, AddressOf godHand
         txt変換元.Select()
     End Sub
     Private Sub KanjiToKana()
 
         Dim objExcel As New Microsoft.Office.Interop.Excel.Application
 
-
-
         Dim stKanji As String = txt変換元.Text
 
         Dim stKana As String
-
-
 
         stKana = objExcel.GetPhonetic(stKanji)
         txt結果.Text = stKana
         MessageBox.Show(stKana, "結果", MessageBoxButtons.OK)
 
     End Sub
-
+    Private Sub godHand()
+        btn変換.PerformClick()
+    End Sub
     Private Sub btn変換_Click(sender As Object, e As EventArgs) Handles btn変換.Click
         Call KanjiToKana()
     End Sub
@@ -37,8 +37,19 @@ Public Class frmふりがな自動入力_EXCEL使用
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
             AddHandler Button2.Click, AddressOf myclick
         End Sub
-        Private Sub myclick(sender As Object, e As EventArgs)
-            MsgBox(sender.name)
-        End Sub
+    Private Sub myclick(sender As Object, e As EventArgs)
+        MsgBox(sender.name)
+    End Sub
+    Private Sub frmふりがな自動入力_EXCEL使用_KeyDown(ByVal sender As Object,
+                                     ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            'If e.Control = False Then
+            '    '[Enter]キーで次の TabIndex があるコントロールへフォーカスを移す
+            '    Me.SelectNextControl(Me.ActiveControl, Not e.Shift, True, True, True)
+            'End If
+            Call godHand()
+        End If
+    End Sub
+
 
 End Class
